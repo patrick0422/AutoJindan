25a685e565d5cde8fd95bc830e658d67f7e8289b
patrick0422 <didxodnd0422@gmail.com>
2021-04-16 08:13:45 +0900

    ElementClickInterceptedException 이슈 죽이기
    Traceback (most recent call last):
      File "AutoJindan.py", line 161, in <module>
        driver.find_element_by_xpath('//*[@id="survey_q1a1"]').click()
      File "selenium\webdriver\remote\webelement.py", line 80, in click
      File "selenium\webdriver\remote\webelement.py", line 633, in _execute
      File "selenium\webdriver\remote\webdriver.py", line 321, in execute
      File "selenium\webdriver\remote\errorhandler.py", line 242, in check_response
    selenium.common.exceptions.ElementClickInterceptedException: Message: element click intercepted: Element <input type="radio" id="survey_q1a1" name="survey_q1" value="1"> is not clickable at point (64, 106). Other element would receive the click: <strong id="title_label" aria-hidden="true">...</strong>
      (Session info: headless chrome=90.0.4430.72)
    
    [15300] Failed to execute script AutoJindan

--- bin/Debug/net5.0-windows/AutoJindan.py
+++ bin/Debug/net5.0-windows/AutoJindan.py
@@ -25,7 +25,7 @@ URL = 'https://hcs.eduro.go.kr/#/loginHome'
 
 options = webdriver.ChromeOptions()
 # 헤드리스 모드로 동작
-# options.add_argument("headless")
+options.add_argument("headless")
 # 불필요한 로그 비활성화
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
@@ -66,10 +66,6 @@ PASSWORD = file.readline().strip()
 print(f'비밀번호: [{PASSWORD}]\n')
 
 file.close()
-
-# NAME = '양태웅'
-# DAY_OF_BIRTH = '040422'
-# PASSWORD = '0422'
 
 #endregion
 
@@ -155,6 +151,7 @@ while True:
     sleep(0.5)
     item.find_element_by_class_name('name').click()
     print(f'{name}학생의 자가진단을 시작합니다.')
+    sleep(0.5)
 
     # 조사 응답
     wait.until(EC.element_to_be_clickable((By.ID, 'survey_q1a1')))
